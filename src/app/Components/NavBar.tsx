f1514889184703efaca982f6c970e1766d0fbc09
ITLcitDepartment <150114649+psantipap@users.noreply.github.com>
2025-04-03 15:26:00 +0700

    Update NavBar.tsx

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -67,7 +67,7 @@ function NavBar() {
                                 className={`absolute left-0 mt-2 bg-white shadow-lg rounded-md w-40 transition-all ${dropdownPokeDex ? "block" : "hidden"
                                     }`}
                             >
-                                {PokeDex.map((pokedex, index) => (
+                                {PokeDex.map((pokedex) => (
                                     <Link href="/project2" className="block px-4 py-2 hover:bg-gray-100">
                                         {pokedex.name}
                                     </Link>
